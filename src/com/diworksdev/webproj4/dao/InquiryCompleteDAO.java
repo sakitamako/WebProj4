9900cc96c071941d254e9532920b999d0d4bc1cc
sakitamako <sa.919.1025.ki@gmail.com>
2024-06-16 16:34:23 +0900

    修正

--- src/com/diworksdev/webproj4/dao/InquiryCompleteDAO.java
+++ src/com/diworksdev/webproj4/dao/InquiryCompleteDAO.java
@@ -33,14 +33,29 @@ public class InquiryCompleteDAO {
 		//try.catchはjavaの例外処理のための構文
 		try {
 
+			//tryの中にはエラーが発生しそうな処理を書く
+			//⑤PreparedStatement（DBまで運んでくれる箱のイメージ）に代入
+			//定義したSQL文の1番目の?にActionから送られたname、
+			//2番目の?にActionから送られたpasswordがそれぞれ入る
 			PreparedStatement ps = con.prepareStatement(sql);
 
+			//⑦executeQuery()/executeUpdate()で実行
+			//（select文の場合はexectuteQuery()を使う）
+			// select文のSQL文を実行するメソッドで戻り値はResultSet
 			ResultSet rs = ps.executeQuery();
 
+			//ここでは2つのことをしている
+			//下に1行ずらすこと
+			//データが存在している限り表示する
 			while(rs.next()) {
 
-				InquiryDTO dto=new InquiryDTO(); dto.setName(rs.getString("name"));
+				//インスタンス化
+				InquiryDTO dto = new InquiryDTO();
 
+				//⑧結果の処理（select文で取得した値をDTOに格納）
+				//select文でDBから取得した情報をString型に変換してDTOクラスに格納
+				//LoginDTOクラスのsetName、setPassword（setter）を利用
+				dto.setName(rs.getString("name"));
 				dto.setQtype(rs.getString("qtype"));
 				dto.setBody(rs.getString("body"));
 
@@ -48,27 +63,44 @@ public class InquiryCompleteDAO {
 
 			}
 
+		//DB接続を終了する際、必ず書くメソッド
+		//最後に実行されるものを指定するための構文
+		//例外が発生しcatchされてもされなくても、共通してやってほしい処理や、やらなければいけない処理を書くところ。
 		} catch (SQLException e) {
 			e.printStackTrace();
 
 		}
 
+		//try.catchはjavaの例外処理のための構文
 		try {
 			con.close();
 
+		//DB接続を終了する際、必ず書くメソッド
+		//最後に実行されるものを指定するための構文
+		//例外が発生しcatchされてもされなくても、共通してやってほしい処理や、やらなければいけない処理を書くところ。
 		} catch (SQLException e) {
 			e.printStackTrace();
 
 		}
 
+		//戻り値
+		//処理結果を返す
 		return inquiryDTOList;
 
 	}
 
+	//①クラス、メソッドの定義
+	//LoginDTO型を最後に呼び出し元に渡すので、LoginDTO型を戻り値にしたメソッドを作る
+	//Actionクラスの値を引数として受け取る
 	public int insert(String name, String qtype, String body) {
 		int ret = 0;
 
+		//②DBConnectorのインスタンス化
+		//DBへの接続準備、DBと会話するためのコード、これでログインできる
+		//Connectionは特定のデータベースとの接続
 		DBConnector db = new DBConnector();
+
+		//③getConnectionの呼び出し（DBと接続する
 		Connection con = db.getConnection();
 
 		String sql = "insert into inquiry values(?,?,?)";
